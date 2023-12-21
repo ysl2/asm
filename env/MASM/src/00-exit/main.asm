32f3d6efe920cd9b58db3595a0b7de799f4fae4f
ysl2 <www.songli.yu@gmail.com>
2023-12-21 18:46:21 +0800

    Add 5_11 and 5_12

--- env/MASM/src/00-exit/main.asm
+++
@@ -1,7 +0,0 @@
-assume cs:code
-code segment
-    ; Use the following code to exit safely.
-    mov ax,4C00h
-    int 21h
-code ends
-end
---
+++ env/MASM/src/5_11/main.asm
@@ -0,0 +1,24 @@
+; Print helloworld
+assume cs:code,ds:data,ss:stack
+
+data segment
+    str db 'hello world','$'
+data ends
+
+stack segment
+    db 10 dup (0)
+stack ends
+
+code segment
+    start:
+        mov ax,data
+        mov ds,ax
+        lea dx,str
+        mov ah,9
+        int 21h
+
+        mov ah,4Ch
+        int 21h
+code ends
+
+end start
